3ce04f5f5c0478b850fd2c2637f5d6d0aeda46ea
Leandro <lelv375@gmail.com>
2017-09-02 18:34:42 -0300

    Created asteroid and added collision logic

---
+++ Assets/Scripts/AsteroidCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidCollider : MonoBehaviour {
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy(gameObject);
+		Destroy(other.gameObject);
+	}
+}
